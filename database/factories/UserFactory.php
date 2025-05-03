b7cdbf4c16876147785b218535a1c95f6e844bf7
Charles Chadwick <charles@mutuallyassured.com>
2025-05-03 09:15:32 -0700

    Seeding the database with users.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,32 +2,42 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
+use App\Enums\UserStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Models\User;
 
-class UserFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
+class UserFactory extends Factory {
+	/**
+	 * The name of the factory's corresponding model.
+	 *
+	 * @var string
+	 */
+	protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     */
-    public function definition(): array
-    {
-        return [
-            'role' => fake()->regexify('[A-Za-z0-9]{25}'),
-            'status' => fake()->regexify('[A-Za-z0-9]{25}'),
-            'first_name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => fake()->dateTime(),
-            'password' => fake()->password(),
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 */
+	public function definition() : array {
+		$created_at = fake()->dateTimeBetween('-1 year', '-1 day');
+		$random_status = rand(1, 25);
+
+		return [
+			'role'              => UserRole::User,
+			'status'            => match(true) {
+				$random_status <= 3 => UserStatus::Inactive,
+				$random_status >= 3 && $random_status <= 6 => UserStatus::Banned,
+				default => UserStatus::Active,
+			},
+			'first_name'        => fake()->firstName(),
+			'last_name'         => fake()->lastName(),
+			'email'             => fake()->safeEmail(),
+			'email_verified_at' => $created_at,
+			'created_at'        => $created_at,
+			'updated_at'        => $created_at,
+			'password'          => Hash::make('password'),
+		];
+	}
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserStatus;
+use App\Models\User;
+use DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+		DB::table('users')->delete();
+
+		$path = database_path('src/characters.csv'); // Ensure the file is in the `database/seeders` directory
+
+		if ( file_exists($path) ) {
+			$file = fopen($path, 'r');
+			$index = 0;
+
+			while ( ( $row = fgetcsv($file) ) !== false ) {
+
+				$row = collect($row)->map(function ( $item ) {
+					return trim($item);
+				});
+
+				$first = $row[ 0 ];
+				$middle = $row[ 1 ];
+				$last = $row[ 2 ];
+				$email = strtolower($first.'.'.str_replace(' ', '-', $last).'@example.com');
+
+				User::factory()->create([
+					'first_name'        => trim($first.' '.$middle),
+					'last_name'         => $last,
+					'email'             => $email,
+				]);
+			}
+
+			fclose($file);
+		}
+    }
+}
