4d26c874e84aac246e3218e0178e6b7fcb5b6b46
Charles Chadwick <charles@mutuallyassured.com>
2025-05-03 12:21:59 -0700

    Added relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
@@ -45,6 +46,13 @@ class Post extends Model {
 	 */
 	public function user() : BelongsTo {
 		return $this->belongsTo(User::class);
+	}
+
+	/**
+	 * @return HasMany
+	 */
+	public function comments() : HasMany {
+		return $this->hasMany(Comment::class);
 	}
 
 	/**
