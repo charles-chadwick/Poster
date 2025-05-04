4fd7d926de1378f6581e3e3eb1ff4137a93b008d
Charles Chadwick <charles@mutuallyassured.com>
2025-05-04 09:20:45 -0700

    Starting the blade stuff over again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,4 @@
 @extends("app", ["title" => "Hi"])
 @section("content")
-    <ul class="divide-stone-300 divide-y">
-        @forelse($posts as $post)
-            <li class="p-4">
-                <x-post :post="$post" />
-            </li>
-        @empty
-            <li>There are no posts right now.</li>
-        @endforelse
-    </ul>
+
 @endsection
